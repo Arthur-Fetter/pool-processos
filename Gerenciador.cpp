4a2fe7766e01f7ba5dc32561794ca93824c9f371
Helena Rocha <helenamartinsrocha07@gmail.com>
2025-06-06 21:26:29 -0300

    modify class Gerenciador

--- Gerenciador.cpp
+++ Gerenciador.cpp
@@ -13,6 +13,7 @@ void Gerenciador::run() {
     while(rodando) {
         escolha = 0;
         while (escolha < 1 || escolha > 6) {
+            cout << string(100, '\n');
             cout << "Gerenciador de processos" << endl;
             cout << "1 - Inserir Processo na Fila" << endl;
             cout << "2 - Executar proximo processo da fila" << endl;
@@ -161,8 +162,7 @@ void Gerenciador::run() {
         default:
             break;
         }
-
-        cout << string(100, '\n');
+        cin.ignore();
     }
 }
 
--- Makefile
+++ Makefile
@@ -9,3 +9,14 @@ build-and-run-mac: build-mac run-mac
 debug-mac:
 	clang++ -g *.cpp -std=c++17 -o ./debug/output
 	lldb ./debug/output
+
+build-linux: 
+	g++ -I . *.cpp -std=c++17 -o ./bin/output
+
+run-linux:
+	./bin/output
+
+build-and-run-linux: build-linux run-linux
+
+debug-linux:
+	g++ -g -I . *.cpp -std=c++17 -o ./debug/output
